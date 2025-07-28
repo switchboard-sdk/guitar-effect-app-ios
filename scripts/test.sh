5bca53c49d0cbfd7e0b4d553e75ee213c04ef514
Balázs Kiss <dev@balazskiss.name>
2025-07-28 11:55:45 +0200

    Updated ios version for tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ set -eu
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 XCODE_PROJECT_PATH="${PROJECT_DIR}/GuitarEffectApp.xcodeproj"
 SCHEME_NAME="GuitarEffectApp"
-DESTINATION="platform=iOS Simulator,name=iPhone 15"
+DESTINATION="platform=iOS Simulator,name=iPhone 16"
 
 xcodebuild clean test \
   -project "$XCODE_PROJECT_PATH" \
